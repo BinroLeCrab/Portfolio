4236e3a002633e2e3a741fa39fd3eec0eba0f98b
Robin Vigier / B!nro <rob.vigier@gmail.com>
2023-10-16 15:57:45 +0200

    :poop: trifouillage svg

--- script/script.js
+++ script/script.js
@@ -2,34 +2,34 @@ window.addEventListener("load",run,false);
 
 function run() {
     
-    // const svg = document.querySelector("svg");
-    // const body = document.querySelector("body");
-    // const yeux = document.querySelector("#svgYeux");
-    // const walk = 100;
+    const svg = document.querySelector("svg");
+    const body = document.querySelector("body");
+    const yeux = document.querySelector("#svgYeux");
+    const walk = 100;
 
-    // function regard(e) {
-    //     console.log("a");
+    function regard(e) {
+        console.log("a");
 
-    //     const { offsetWidth: width, offsetHeight: height } = body;
-    //     let { offsetX: x, offsetY: y } = e;
+        const { offsetWidth: width, offsetHeight: height } = body;
+        let { offsetX: x, offsetY: y } = e;
 
-    //     console.log(width, height)
+        console.log(width, height)
 
-    //     console.log(x,y)
+        console.log(x,y)
 
-    //     if (this !== e.target) {
-    //         x = x + e.target.offsetLeft;
-    //         y = y + e.target.offsetTop;
-    //     }
+        if (this !== e.target) {
+            x = x + e.target.offsetLeft;
+            y = y + e.target.offsetTop;
+        }
 
-    //     const xWalk = Math.round((x / width * walk) - (walk / 2));
-    //     const yWalk = Math.round((y / height * walk) - (walk / 2));
+        const xWalk = Math.round((x / width * walk) - (walk / 2));
+        const yWalk = Math.round((y / height * walk) - (walk / 2));
 
-    //     yeux.style.transform = `
-    //         translate(${xWalk}px, ${yWalk}px)
-    //     `;
+        yeux.style.transform = `
+            translate(${xWalk}px, ${yWalk}px)
+        `;
 
-    // } 
+    } 
 
-    // body.addEventListener('mousemove', regard);
+    body.addEventListener('mousemove', regard);
 }
