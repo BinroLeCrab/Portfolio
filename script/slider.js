c10c00b3fe8f8a0dba6830c826f819e2cadbf800
Robin Vigier / B!nro <rob.vigier@gmail.com>
2023-12-12 21:50:30 +0100

    🚧 Animation slider

--- script/slider.js
+++ script/slider.js
@@ -7,6 +7,26 @@ let Gap = window.getComputedStyle(slider, null).gap;
 Gap = parseInt(Gap.substring(0, Gap.length - 2));
 let walkSlider = Cards + Gap;
 
+// function AnimR() {
+//     const tl = gsap.timeline();
+
+//     tl.to('.CardsProj',{
+//         transform: "rotate(-10Deg)",
+//         duration: 0.15,
+//         ease: "back.in"
+//     })
+
+//     tl.to('.CardsProj',{
+//         transform: "rotate(-10Deg)",
+//         duration: 0.35
+//     })
+
+//     tl.to('.CardsProj',{
+//         transform: "rotate(0Deg)",
+//         duration: 0.3,
+//         ease: "back.out"
+//     })
+// }
 
 right.addEventListener("click", ToTheRight);
 left.addEventListener("click", ToTheLeft);
@@ -23,6 +43,7 @@ function ToTheLeft() {
     if (arrivée > 0 || l == 0) {
         console.log("retour origine");
         slider.style.left = `-${slider.offsetWidth - Cards}px`;
+        // Anim();
     } else {
         slider.style.left = `${l + walkSlider}px`;
     }
@@ -43,8 +64,10 @@ function ToTheRight() {
     if (arrivée < max || l == max) {
         console.log("retour droite");
         slider.style.left = `0px`;
+        // AnimR();
     } else {
         slider.style.left = `${l - walkSlider}px`;
+        // AnimR();
     }
 }
 
