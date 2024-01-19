04ccd095b370bd2a9839736070fbbd85e38c7621
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-01-19 10:43:32 +0100

    :ambulance: CVVV

--- script/script.js
+++ script/script.js
@@ -1,8 +1,6 @@
 //// document.addEventListener("unload", (event) => {console.log(event)});
 
-window.addEventListener("load",run,false);
-
-function run() {
+// window.addEventListener("load",run,false);
     
     // const svg = document.querySelector("svg");
     const body = document.querySelector("body");
@@ -55,48 +53,48 @@ function run() {
 
     };
 
-    const OverBtn = document.querySelector("#OverCircle");
-    let clic = 0;
+    // const OverBtn = document.querySelector("#OverCircle");
+    // let clic = 0;
 
-    OverBtn.addEventListener("click", ClickRes);
+    // OverBtn.addEventListener("click", ClickRes);
 
-    document.querySelector("main").addEventListener("click", () => {
+    // document.querySelector("main").addEventListener("click", () => {
 
-        if (clic ==1) {
-            ClickRes();
-        }
+    //     if (clic ==1) {
+    //         ClickRes();
+    //     }
 
-    });
+    // });
 
-    function ClickRes(){
-        const Res = document.querySelector("#ResOver");
+    // function ClickRes(){
+    //     const Res = document.querySelector("#ResOver");
 
-        if(clic == 0) {
-            Res.classList.remove("ResNoClick");
-            Res.classList.add("ResClick");
+    //     if(clic == 0) {
+    //         Res.classList.remove("ResNoClick");
+    //         Res.classList.add("ResClick");
             
-            clic = 1;
-        } else {
-            Res.classList.remove("ResClick");
-            Res.classList.add("ResNoClick");
+    //         clic = 1;
+    //     } else {
+    //         Res.classList.remove("ResClick");
+    //         Res.classList.add("ResNoClick");
 
-            clic = 0;
-        }
+    //         clic = 0;
+    //     }
 
-    }
+    // }
 
-    let helicoO = document.querySelector("#helicoO");
-    let helicoT = document.querySelector("#helicoT");
-    let DegO = 0;
-    let DegT = 180;
+    // let helicoO = document.querySelector("#helicoO");
+    // let helicoT = document.querySelector("#helicoT");
+    // let DegO = 0;
+    // let DegT = 180;
 
-    body.addEventListener("wheel", () => {
-        DegO = DegO + 50;
-        DegT = DegT + 50;
-        helicoO.style.transform = `rotateY(${DegO}deg)`;
-        helicoT.style.transform = `rotateY(${DegT}deg)`;
-    });
-}
+    // body.addEventListener("wheel", () => {
+    //     DegO = DegO + 50;
+    //     DegT = DegT + 50;
+    //     helicoO.style.transform = `rotateY(${DegO}deg)`;
+    //     helicoT.style.transform = `rotateY(${DegT}deg)`;
+    // });
+
 
 /*   _____     _____
     |  _  |   |  _  |
